501eaf2e8d577bb9ee4527dc2e2bf2e8c7d25bfa
Nikita <nikita.kuznetsov.r@gmail.com>
2023-05-25 17:42:25 +0300

    змейка готова

---
+++ components/Snake/Snake.vue
@@ -0,0 +1,178 @@
+<template>
+    <div class="snake">
+        <div class="snake__body">
+            <div
+                class="snake__item"
+                v-for="(item, index) in field"
+                :key="index"
+                :class="{
+                    active: snake.includes(index) || index === food,
+                }"
+            >
+                <span>{{ index }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const field = ref(new Array(256))
+const snake = ref([1, 2, 3, 4])
+const borderTop = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+const borderRight = [
+    15, 31, 47, 63, 79, 95, 111, 127, 143, 159, 175, 191, 207, 223, 239, 255,
+]
+const borderBottom = [
+    240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254,
+    255,
+]
+const borderLeft = [
+    0, 16, 32, 48, 64, 80, 96, 112, 128, 144, 160, 176, 192, 208, 224, 240,
+]
+
+// конец игры
+const gameOver = ref(false)
+
+const start = () => {
+    let foods = []
+    let tick = setInterval(() => {
+        lengthSnake()
+
+        snake.value.filter((number, index, numbers) => {
+            if (numbers.indexOf(number) !== index) gameOver.value = true
+        })
+
+        if (gameOver.value) {
+            alert('проиграли')
+            clearInterval(tick)
+        }
+
+        // если скушали вкусняшку
+        if (snake.value[0] === food.value) {
+            foods.push(food.value)
+            food.value = randomFood()
+        }
+
+        // када еда дойдет до хвостика, змейка увеличивается
+        if (snake.value[snake.value.length - 1] === foods[0]) {
+            snake.value.push(foods[0])
+            foods.splice(0, 1)
+        }
+    }, 100)
+}
+
+// start()
+
+// хрен пойми что я тут написал но это работает)
+// короче, эта функция передает элементам массива змейки значение соседнего элемента
+const lengthSnake = () => {
+    let oldCount = snake.value[0]
+    let newCount = snake.value[0]
+
+    // указываем куда двигаться
+    snake.value[0] = direction(directionRun.value)
+
+    for (let i = 1; i < snake.value.length; i++) {
+        oldCount = snake.value[i]
+        snake.value[i] = newCount
+        newCount = oldCount
+    }
+}
+
+// рандомная вкусняшка
+let food = ref(randomFood())
+
+// направление
+const directionRun = ref('ArrowLeft')
+
+// рандомное число
+function randomFood() {
+    let count = Math.floor(Math.random() * 256)
+    return snake.value.includes(count) ? randomFood() : count
+}
+
+// задаем направление
+const direction = (directionRun) => {
+    // если движемся вверх
+    if (directionRun === 'ArrowUp') {
+        if (borderTop.includes(snake.value[0])) {
+            return (snake.value[0] += borderBottom[0])
+        }
+        return (snake.value[0] -= 16)
+    }
+
+    // если движемся вправо
+    if (directionRun === 'ArrowRight') {
+        if (borderRight.includes(snake.value[0])) {
+            return (snake.value[0] -= borderRight[0])
+        }
+        return (snake.value[0] += 1)
+    }
+
+    // если движемся вниз
+    if (directionRun === 'ArrowDown') {
+        if (borderBottom.includes(snake.value[0])) {
+            return (snake.value[0] -= borderBottom[0])
+        }
+        return (snake.value[0] += 16)
+    }
+
+    // если движемся влево
+    if (directionRun === 'ArrowLeft') {
+        if (borderLeft.includes(snake.value[0])) {
+            return (snake.value[0] += borderLeft.length - 1)
+        }
+        return (snake.value[0] -= 1)
+    }
+}
+
+// запрещаем движение змейки в протвоположное направление
+document.addEventListener('keydown', (event) => {
+    if (directionRun.value === 'ArrowUp' && event.key === 'ArrowDown') return
+    if (directionRun.value === 'ArrowRight' && event.key === 'ArrowLeft') return
+    if (directionRun.value === 'ArrowDown' && event.key === 'ArrowUp') return
+    if (directionRun.value === 'ArrowLeft' && event.key === 'ArrowRight') return
+    directionRun.value = event.key
+})
+</script>
+
+<style lang="scss" scoped>
+.snake {
+    position: relative;
+    background-color: #000;
+    padding-top: 100%;
+
+    &__body {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        display: grid;
+        grid-template-rows: repeat(16, 1fr);
+        grid-template-columns: repeat(16, 1fr);
+    }
+
+    &__item {
+        position: relative;
+        color: #fff;
+        font-size: 12px;
+
+        &.active {
+            background-color: yellow;
+            color: #000;
+        }
+
+        & span {
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    }
+}
+</style>
